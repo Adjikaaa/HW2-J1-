41dc2920f8066149cad3a3e3d3f08ec5e130d460
Mariya Kokhonova <adjika88@gmail.com>
2022-01-30 12:33:54 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-      double amount = 78500.37;
-      int bonus = (int) amount/20;
+        double amount = 78500.37;
+        int bonus = (int) amount / 20;
         System.out.println("Количество начисленных мили за билет:" + bonus);
-
-        // тут описание того что будет выполняться
-        // при выполнении метода main
     }
 }
