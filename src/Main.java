de6d47e1bce301f3a80ea7e2f2e68449bf304f6e
Mariya Kokhonova <adjika88@gmail.com>
2022-01-30 12:29:26 +0300

    Java added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+      double amount = 78500.37;
+      int bonus = (int) amount/20;
+        System.out.println("Количество начисленных мили за билет:" + bonus);
+
+        // тут описание того что будет выполняться
+        // при выполнении метода main
+    }
+}
